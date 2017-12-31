f2bc475dbfbab3dca9f111f8eaf4c63c050326a2
jjatem <jjatem@hotmail.com>
2017-12-31 13:15:58 -0500

    more sqllite3 item related changes

--- section5/code/item.py
+++ section5/code/item.py
@@ -63,5 +63,19 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    TABLE_NAME = 'items'
+
     def get(self):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
+        result = cursor.execute(query)
+
+        items = []
+        for row in result:
+            items.append({'name': row[0], 'price': row[1]})
+
+        connection.close()
+
         return {'items': items}
