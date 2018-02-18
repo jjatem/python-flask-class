9aa8f8dce6f351417fa5d88d7ad32452ad3517fb
jjatem <jjatem@hotmail.com>
2018-02-17 23:15:21 -0500

    Adding Store resource and model. Store methods.

--- section6/code/resources/store.py
+++ section6/code/resources/store.py
@@ -31,4 +31,4 @@ class Store(Resource):
 
 class StoreList(Resource):
     def get(self):
-        return {'stores': [store.json() for store in StoreModel.query.all()]}
+        return {'stores': list(map(lambda x: x.json(), StoreModel.query.all()))}
